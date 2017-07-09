1af05ddae8a2a461291ed0ed60d5b9b0574853d1
Freitas <freitas@Freitass-MacBook-Air.local>
2017-07-09 19:25:44 -0300

    Troca da configuracao do banco apontando para minha aplicacao firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
-
+import { initializeApp, database } from 'firebase';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA5PDvF3DaxXXcC-zJoxGiy-1Mw3W9iFLY",
+      authDomain: "angularfirebasecursoudemyvasco.firebaseapp.com",
+      databaseURL: "https://angularfirebasecursoudemyvasco.firebaseio.com",
+      projectId: "angularfirebasecursoudemyvasco",
+      storageBucket: "angularfirebasecursoudemyvasco.appspot.com",
+      messagingSenderId: "1040350357505"
+    };
+    firebase.initializeApp(config);
+
+    //Associa o DB com o root node
+    var root = firebase.database().ref();
+    root.on('value', function (snap) {
+      console.log(snap.val());
+    });
+  }
 
 
 }
--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -3,11 +3,17 @@
 
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyA0BcUcu4V8aHT_gM-32BhRcmqji4z-lts",
-    authDomain: "final-project-recording.firebaseapp.com",
-    databaseURL: "https://final-project-recording.firebaseio.com",
-    storageBucket: "final-project-recording.appspot.com",
-    messagingSenderId: "290354329688"
-};
+    // apiKey: "AIzaSyA0BcUcu4V8aHT_gM-32BhRcmqji4z-lts",
+    // authDomain: "final-project-recording.firebaseapp.com",
+    // databaseURL: "https://final-project-recording.firebaseio.com",
+    // storageBucket: "final-project-recording.appspot.com",
+    // messagingSenderId: "290354329688"
 
+      apiKey: "AIzaSyA5PDvF3DaxXXcC-zJoxGiy-1Mw3W9iFLY",
+      authDomain: "angularfirebasecursoudemyvasco.firebaseapp.com",
+      databaseURL: "https://angularfirebasecursoudemyvasco.firebaseio.com",
+      storageBucket: "angularfirebasecursoudemyvasco.appspot.com",
+      messagingSenderId: "1040350357505"
+
+};
 
